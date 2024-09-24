f7cf227d14c6bc38031bd71f473b9772b43a1e1a
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-09-24 03:42:22 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TraffictLight from './TraffictLight'
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <TraffictLight /> 
+
+  </React.StrictMode>,
+)
